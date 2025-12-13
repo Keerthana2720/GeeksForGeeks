92bf046349fc00820d233730d2bd0f6007e4f3d4
Keerthana2720 <165762087+Keerthana2720@users.noreply.github.com>
2025-12-13 11:53:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Search Pattern/search-pattern.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+
+class Solution {
+    ArrayList<Integer> search(String pat, String txt) {
+        int m = pat.length();
+        int n = txt.length();
+
+        int[] lps = new int[m];
+        ArrayList<Integer> res = new ArrayList<>();
+
+        constructLps(pat, lps);
+
+        int i = 0; // index for txt
+        int j = 0; // index for pat
+
+        while (i < n) {
+            if (txt.charAt(i) == pat.charAt(j)) {
+                i++;
+                j++;
+                if (j == m) {
+                    res.add(i - j); // match found
+                    j = lps[j - 1];
+                }
+            } else {
+                if (j != 0) {
+                    j = lps[j - 1];
+                } else {
+                    i++;
+                }
+            }
+        }
+        return res;
+    }
+
+    public void constructLps(String pat, int[] lps) {
+        int len = 0;
+        lps[0] = 0;
+        int i = 1;
+
+        while (i < pat.length()) {
+            if (pat.charAt(i) == pat.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+    }
+}
