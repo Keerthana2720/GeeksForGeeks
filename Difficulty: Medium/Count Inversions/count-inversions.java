6088eecb26276f9a01429fedfd2b01a6537eb2ea
Keerthana2720 <165762087+Keerthana2720@users.noreply.github.com>
2026-01-02 11:41:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Inversions/count-inversions.java
@@ -0,0 +1,52 @@
+class Solution {
+
+    static int inversionCount(int arr[]) {
+        int n = arr.length;
+        return countInv(arr, 0, n - 1);
+    }
+
+    static int countInv(int[] arr, int l, int r) {
+        int res = 0;
+        if (l < r) {
+            int m = l + (r - l) / 2;
+            res += countInv(arr, l, m);
+            res += countInv(arr, m + 1, r);
+            res += countAndMerge(arr, l, m, r);
+        }
+        return res;
+    }
+    static int countAndMerge(int[] arr, int left, int mid, int right) {
+
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        for (int i = 0; i < n1; i++)
+            L[i] = arr[left + i];
+
+        for (int j = 0; j < n2; j++)
+            R[j] = arr[mid + 1 + j];
+
+        int i = 0, j = 0, k = left;
+        int invCount = 0;
+
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k++] = L[i++];
+            } else {
+                arr[k++] = R[j++];
+                invCount += (n1 - i); 
+            }
+        }
+
+        while (i < n1)
+            arr[k++] = L[i++];
+
+        while (j < n2)
+            arr[k++] = R[j++];
+
+        return invCount;
+    }
+}
